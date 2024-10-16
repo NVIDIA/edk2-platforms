3c712a7179ff45cfefd2d2375c9fe61dbc750589
Nhi Pham <nhi@os.amperecomputing.com>
2024-10-16 10:53:57 +0700

    AmpereSiliconPkg: Add Ampere defined Variables GUID
    This introduces a GUID for Ampere defined variables.
    
    Signed-off-by: Nhi Pham <nhi@os.amperecomputing.com>

---
+++ Silicon/Ampere/AmpereSiliconPkg/Include/Guid/AmpereVariableGuid.h
@@ -0,0 +1,19 @@
+/** @file
+
+  Copyright (c) 2024, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef AMPERE_VARIABLE_GUID_H_
+#define AMPERE_VARIABLE_GUID_H_
+
+#define AMPERE_VARIABLE_GUID \
+  { \
+    { 0x6B9F9381, 0xB1D0, 0x4991, { 0xBC, 0x4A, 0x17, 0xBC, 0x6B, 0x5B, 0xB0, 0x81 } } \
+  }
+
+extern EFI_GUID  gAmpereVariableGuid;
+
+#endif /* AMPERE_VARIABLE_GUID_H_ */

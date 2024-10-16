a61061c79c2ae88807ad2545d2c6964aa0764441
Nhi Pham <nhi@os.amperecomputing.com>
2024-10-16 10:57:09 +0700

    AmpereSiliconPkg: Add Ampere defined After Console Event GUID
    This introduces a GUID for the Ampere defined AfterConsole event, which
    serves as a notification that AfterConsole is ready before ReadyToBoot.
    This event is used to handle the IPMI boot order change request from
    BMC.
    
    Signed-off-by: Nhi Pham <nhi@os.amperecomputing.com>

---
+++ Silicon/Ampere/AmpereSiliconPkg/Include/Guid/AmpereEventAfterConsole.h
@@ -0,0 +1,19 @@
+/** @file
+
+  Copyright (c) 2024, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef AMPERE_EVENT_AFTER_CONSOLE_H_
+#define AMPERE_EVENT_AFTER_CONSOLE_H_
+
+#define AMPERE_EVENT_AFTER_CONSOLE_GUID \
+  { \
+    0x7CCB608E, 0x428C, 0x44AE, { 0x95, 0x06, 0x58, 0x58, 0x2F, 0x0E, 0x8F, 0xB1 } \
+  }
+
+extern EFI_GUID  gAmpereEventAfterConsoleGuid;
+
+#endif /* AMPERE_EVENT_AFTER_CONSOLE_H_ */

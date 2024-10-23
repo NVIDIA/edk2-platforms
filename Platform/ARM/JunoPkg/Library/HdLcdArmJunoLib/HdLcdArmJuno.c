ea33b279488f719a9103b81369c626eeea7a3756
Mike Beaton <mjsbeaton@gmail.com>
2024-10-23 23:15:05 +0100

    Platform/ARM/JunoPkg: Refactor HdLcdArmJuno.c to use DEBUG_CODE_BEGIN/END
    In general, it is preferable to avoid code explicitly referencing
    MDEPKG_NDEBUG outside the shared DebugLib headers. edk2 commit
    ae83c6b7fd83a5906e016a32027c1bcd792a624e makes this considerably easier,
    because static functions and variables which are only referenced within
    debug macros no longer give warnings (they are understood as referenced
    but unused). MDEPKG_NDEBUG dependent code which was previously required
    to comment out such variables for RELEASE builds now both can and must
    be removed (see immediately prior edk2-platforms commit).
    
    By contrast, in the case of the update here, since both the STATIC
    function and the code which references it are wrapped in MDEPKG_NDEBUG
    conditional code, there is no break in the RELEASE build when the 'null'
    macros definitions are updated.
    
    However, we can still avoid yet more MDEPKG_NDEBUG dependent code here
    by refactoring to use DEBUG_CODE_BEGIN and DEBUG_CODE_END. This is
    considered preferable as it allows these debug macros to wrap the
    dependency on MDEPKG_NDEBUG, presumably as originally intended.
    
    Signed-off-by: Mike Beaton <mjsbeaton@gmail.com>

--- Platform/ARM/JunoPkg/Library/HdLcdArmJunoLib/HdLcdArmJuno.c
+++ Platform/ARM/JunoPkg/Library/HdLcdArmJunoLib/HdLcdArmJuno.c
@@ -247,7 +247,6 @@ LcdPlatformGetMaxMode (VOID)
   return  mMaxMode;
 }
 
-#if !defined(MDEPKG_NDEBUG)
 /** Probe Clock device ID of the HDLCD clock and current pixel clock frequency.
   NOTE: We will probe information only in DEBUG build.
 
@@ -380,7 +379,6 @@ ProbeHdLcdClock (
 
   return EFI_SUCCESS;
 }
-#endif
 
 /** Set the requested display mode.
 
@@ -426,17 +424,17 @@ LcdPlatformSetMode (
     return Status;
   }
 
-#if !defined(MDEPKG_NDEBUG)
   /* Avoid probing clock device ID in RELEASE build */
+  DEBUG_CODE_BEGIN ();
   Status = ProbeHdLcdClock (ClockProtocol, &ClockId);
   if (EFI_ERROR (Status)) {
     return Status;
   }
 
   ASSERT (ClockId == ARM_JUNO_CSS_CLKID_HDLCD_0);
-#else
+  DEBUG_CODE_END ();
+
   ClockId = ARM_JUNO_CSS_CLKID_HDLCD_0;
-#endif
 
   // Set HDLCD clock required for the requested mode
   Status = ClockProtocol->RateSet (
@@ -449,7 +447,7 @@ LcdPlatformSetMode (
     return Status;
   }
 
-#if !defined(MDEPKG_NDEBUG)
+  DEBUG_CODE_BEGIN ();
   UINT64  CurrentHdLcdFreq;
   // Actual value set can differ from requested frequency so verify.
   Status = ClockProtocol->RateGet (
@@ -465,7 +463,7 @@ LcdPlatformSetMode (
       CurrentHdLcdFreq
       ));
   }
-#endif
+  DEBUG_CODE_END ();
 
   return Status;
 }

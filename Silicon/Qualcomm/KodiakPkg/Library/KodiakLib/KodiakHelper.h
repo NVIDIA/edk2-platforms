0a51cc11dd74be199c67eceabc7bf68d36bb7075
Sumit Garg <sumit.garg@oss.qualcomm.com>
2025-08-14 13:33:39 +0530

    Silicon/Qualcomm/Kodiak: Enable booting edk2 at EL2
    Many Qualcomm targets use Gunyah as a firmware-launched hypervisor, making
    EL2 unavailable for Non-secure firmware and operating systems, preventing
    use of alternative hypervisors.
    
    Quoting Arm Base Boot Requirements here:
    
    6.3.1.1. UEFI boot at EL2
      Systems must boot UEFI at EL2, to enable the installation of a hypervisor
      or a virtualization-aware operating system.
    
    So lets use a TZ SMC call supported on these Qcom targets to clean up
    EL2/Gunyah configuration and transition to booting edk2 (UEFI) at EL2
    exiting Gunyah. All the SMMU configuration gets cleaned up and handed
    over in EL2 in bypass mode. The edk2 is able to configure EL2 MMU/SMMU as
    you would expect on an Arm silicon executing edk2 in EL2.
    
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

---
+++ Silicon/Qualcomm/KodiakPkg/Library/KodiakLib/KodiakHelper.h
@@ -0,0 +1,15 @@
+/** @file
+  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef KODIAK_HELPER_H_
+#define KODIAK_HELPER_H_
+
+#define TZ_EL2_SWITCH_SMC_ID                0x02000121
+#define TZ_EL2_SWITCH_PARAM_ID              0x00000023
+#define TZ_EL2_SWITCH_PARAM2_EXIT_GUNYAH    0x1
+
+#endif /* KODIAK_HELPER_H_ */

29203d6aa8d2197be9401dd96c112ccb320708d0
Raymond Yang <yangrongwei@hotmail.com>
2025-09-01 00:24:08 +0800

    use DEBUG for DXE driver

--- Platform/RaspberryPi/Drivers/FdtDxe/FdtDxe.c
+++ Platform/RaspberryPi/Drivers/FdtDxe/FdtDxe.c
@@ -15,7 +15,6 @@
 #include <Library/FdtLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
-#include <Library/UefiLib.h>
 #include <Protocol/RpiFirmware.h>
 #include <Guid/Fdt.h>
 #include <ConfigVars.h>
@@ -500,37 +499,37 @@ FdtDxeInitialize (
 
   Status = SanitizePSCI ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to sanitize PSCI: %r\n", Status);
+    DEBUG (("Failed to sanitize PSCI: %r\n", Status));
   }
 
   Status = CleanMemoryNodes ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to clean memory nodes: %r\n", Status);
+    DEBUG (("Failed to clean memory nodes: %r\n", Status));
   }
 
   Status = CleanSimpleFramebuffer ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to clean frame buffer: %r\n", Status);
+    DEBUG (("Failed to clean frame buffer: %r\n", Status));
   }
 
   Status = FixEthernetAliases ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to fix ethernet aliases: %r\n", Status);
+    DEBUG (("Failed to fix ethernet aliases: %r\n", Status));
   }
 
   Status = UpdateMacAddress ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to update MAC address: %r\n", Status);
+    DEBUG (("Failed to update MAC address: %r\n", Status));
   }
 
   Status = AddUsbCompatibleProperty ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to update USB compatible properties: %r\n", Status);
+    DEBUG (("Failed to update USB compatible properties: %r\n", Status));
   }
 
   SyncPcie ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to update PCIe address ranges: %r\n", Status);
+    DEBUG (("Failed to update PCIe address ranges: %r\n", Status));
   }
 
   DEBUG ((DEBUG_INFO, "Installed devicetree at address %p\n", mFdtImage));
